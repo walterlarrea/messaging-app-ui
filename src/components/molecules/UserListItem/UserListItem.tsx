44f3b6975f691185cced3b29fd2fe81f504edc3e
Walter Larrea <walterlarrea@live.com>
2024-01-10 21:27:21 -0300

    add: blank profile picture to user list

--- src/components/molecules/UserListItem/UserListItem.tsx
+++ src/components/molecules/UserListItem/UserListItem.tsx
@@ -1,9 +1,20 @@
+import blankProfilePicture from '../../../assets/images/blank-profile-picture-640.png'
+
 interface ItemProps {
 	userName: string
 }
 
 const UserListItem = ({ userName }: ItemProps) => {
-	return <li>{userName}</li>
+	return (
+		<li className="flex flex-nowrap gap-[8px] p-[5px] cursor-pointer bg-slate-200 hover:bg-slate-100 border rounded-lg">
+			<img
+				src={blankProfilePicture.src}
+				alt="Generic blank user picture"
+				className="rounded-full w-[48px]"
+			/>
+			<span className="font-semibold">{userName}</span>
+		</li>
+	)
 }
 
 export default UserListItem
