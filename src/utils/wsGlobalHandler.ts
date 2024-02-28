47742489902792fb4f238b068c6592a0bbcd65ea
Walter Larrea <walterlarrea@live.com>
2024-02-28 15:29:14 -0300

    add: realtime notification for friend requests and friend request approval

--- src/utils/wsGlobalHandler.ts
+++ src/utils/wsGlobalHandler.ts
@@ -2,6 +2,7 @@ import { io } from 'socket.io-client'
 import { API_URI } from '../config/constants'
 import { $auth } from '../store/auth'
 import { addOneCurrentChatMessage } from '../store/chat'
+import { addOneFriendRequest, approvedFriendRequest } from '../store/friends'
 
 const BASE_URL = API_URI
 
@@ -19,6 +20,23 @@ $auth.listen((auth) => {
 		if (res.receiverId !== auth?.userId) return
 
 		addOneCurrentChatMessage(res)
+	})
+
+	SOCKET.off('getFriendRequest')
+	SOCKET.on('getFriendRequest', (res) => {
+		// This should never happen as the backend is targeting the requests
+		// if (res.receiverId !== auth?.userId) return
+
+		addOneFriendRequest(res)
+	})
+
+	SOCKET.off('approvedFriendRequest')
+	SOCKET.on('approvedFriendRequest', (res) => {
+		console.log('aFR', res)
+		// This should never happen as the backend is targeting the requests
+		// if (res.receiverId !== auth?.userId) return
+
+		approvedFriendRequest()
 	})
 })
 
